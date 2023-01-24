7f451390d8116d2e25aad80615a925f08dfcd30d
Sunny <sunny6464n@gmail.com>
2023-01-25 01:53:08 +0530

    initial commit

---
+++ server/index.js
@@ -0,0 +1,58 @@
+require("dotenv").config(); 
+const express = require("express"); 
+const axios = require("axios"); 
+
+const app = express(); 
+
+app.use(express.urlencoded({extended : true})); 
+
+
+app.get("/", (req, res)=>{
+    const country = req.body.country; // provide The 2-letter ISO 3166-1 code of the country
+    let url = `https://newsapi.org/v2/everything?q=everything&apiKey=${process.env.API_KEY}`
+    axios.get(url)
+    .then(response=>{
+        res.json({
+            status : 200, 
+            success : true, 
+            message : "Successfully fetched the data", 
+            data :  response.data
+        })
+    })
+    .catch(function (error) {
+        // handle error
+        res.json({
+            status : 500, 
+            success : true, 
+            message : "Failed to fetch Data form the API", 
+            error : error
+        })
+    })
+})
+
+app.get("/top-headlines", (req, res)=>{
+    const country = req.body.country; // provide The 2-letter ISO 3166-1 code of the country
+    let url = `https://newsapi.org/v2/top-headlines/sources?apiKey=${process.env.API_KEY}`
+    axios.get(url)
+    .then(response=>{
+        res.json({
+            status : 200, 
+            success : true, 
+            message : "Successfully fetched the data", 
+            data : response.data
+        })
+    })
+    .catch(function (error) {
+        // handle error
+        res.json({
+            status : 500, 
+            success : true, 
+            message : "Failed to fetch Data form the API", 
+            error : error
+        })
+    })
+})
+
+app.listen(3000, function(){
+    console.log("Server is running at port 3000"); 
+})
