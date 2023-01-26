c6a82289acb5ec7df4c72948d38fc1061f8da2fb
Sunny <sunny6464n@gmail.com>
2023-01-26 23:51:26 +0530

    fixed the layout

--- client/src/components/Cards.jsx
+++ client/src/components/Cards.jsx
@@ -20,7 +20,7 @@ function Cards() {
   }, [])
 
   return (
-    <div className='cards grid place-items-center md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 xs:grid-cols-1 xs:gap-4 md:gap-10 lg:gap-14 md:px-16 xs:p-3 '>
+    <div className='cards grid lg:place-content-center md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 xs:grid-cols-1 xs:gap-4 md:gap-10 lg:gap-14 md:px-16 xs:p-3 '>
       {data.map((element, index) => {
         return <EverythingCard 
            title ={element.title} description = {element.description} imgUrl ={element.urlToImage}
