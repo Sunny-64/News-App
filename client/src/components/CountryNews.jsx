9bcc282814f8658d7f263d405df60aefdf216e87
Sunny <sunny6464n@gmail.com>
2023-01-28 22:22:07 +0530

    change country feature

--- client/src/components/CountryNews.jsx
+++ client/src/components/CountryNews.jsx
@@ -25,7 +25,7 @@ function CountryNews() {
     .then(myJson=> {
       // console.log(myJson)
       setData(myJson.data.articles)})
-  }, [])
+  },[data])
 
   return (
     <div className='cards grid lg:place-content-center md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 xs:grid-cols-1 xs:gap-4 md:gap-10 lg:gap-14 md:px-16 xs:p-3 '>
--- client/src/components/EverythingCard.jsx
+++ client/src/components/EverythingCard.jsx
@@ -6,13 +6,13 @@ function Card(props) {
     <div className='everything-card flex flex-col p-5 gap-3 mb-5'>
         <p className="title font-medium mb-2">{props.title}</p>
         <div className="everything-card-img mx-auto">
-          <img crossOrigin="anonymous" className="everything-card-img" src={props.imgUrl} alt="img" />
+          <img className="everything-card-img" src={props.imgUrl} alt="img" />
         </div>
         <div className="description">
           <p className="description-text leading-7">{props.description}</p>
         </div>
         <div className="info">
-          <p className="url mb-2"><span className='font-semibold'>url : </span><a href={props.url} target="_blank" className="link underline">{props.url}</a></p>
+          <p className="url mb-2"><span className='font-semibold'>url : </span><a href={props.url} target="_blank" className="link underline">{props.url.substring(0,100)}</a></p>
           <div className="origin flex justify-between flex-wrap">
             <p><span className='font-semibold'>Source :</span>{props.source}</p>
             <p><span className="font-semibold">Author :</span>{props.author}</p>
